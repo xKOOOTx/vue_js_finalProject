99c3295403b019bc1abd76c017bc35749f2e52e9
xKOOOTx <kooot123@gmail.com>
2020-12-15 16:22:32 +0300

    added header and nav components

---
+++ game-store/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app container">
+    <div class="header-background__image">
+      <Header class="center"/>
+      <Nav class="center-small" />
+    </div>
+  </div>
+</template>
+
+<script>
+import '@/assets/styles/main.scss'
+import Header from './components/Header'
+import Nav from './components/Nav'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Nav
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ game-store/src/components/Header.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="">
+    <div class="header">
+      <div class="header__left flex_1">
+        <a href="index.html" class="header__logo_link">
+          <div class="header__logo top">historical</div>
+          <div class="header__logo bottom">games</div>
+        </a>
+      </div>
+      <div class="header__center flex_1">
+        <a href="" class="header__center_phone">
+          +1234567890
+        </a>
+        <a href="" class="header__center_email">
+          info@example.com
+        </a>
+      </div>
+      <div class="header__right flex_1">
+        <form>
+          <input type="text">
+          <button>
+            <i class="fas fa-search"></i>
+          </button>
+        </form>
+      </div>
+    </div>
+    <Nav/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style lang="scss" scoped>
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    height: 8.3rem;
+    margin-top: 10px;
+
+    &__logo {
+      text-transform: capitalize;
+      &_link {
+        display: block;
+      }
+    }
+
+    &__center {
+      justify-content: space-around;
+    }
+    &__right {
+      justify-content: flex-end;
+    }
+  }
+  .fa-search {
+    font-size: 1.2rem;
+  }
+
+</style>
