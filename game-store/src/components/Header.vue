0378e26c9f11cec9ea06d7c680ee46503a71fc98
xKOOOTx <kooot123@gmail.com>
2020-12-17 15:58:03 +0300

    edited Header.vue

--- game-store/src/components/Header.vue
+++ game-store/src/components/Header.vue
@@ -3,16 +3,20 @@
     <div class="header center">
       <div class="header__left flex_1">
         <a href="index.html" class="header__logo_link">
-          <div class="header__logo header__logo_top">historical</div>
-          <div class="header__logo header__logo_bottom">games</div>
+          <p class="header__logo header__logo_top">historical</p>
+          <p class="header__logo header__logo_bottom">games</p>
         </a>
       </div>
       <div class="header__center flex_1">
-        <a href="" class="header__center_phone">
-          +1234567890
+        <a href="#" class="header__center_phone">
+          <img src="../assets/images/header_phone.png" alt="" class="header__center_phone-img">
+          <span class="header__center_phone-number">
+            <p>+123</p><p>456</p><p>7890</p>
+          </span>
         </a>
-        <a href="" class="header__center_email">
-          info@example.com
+        <a href="#" class="header__center_phone">
+          <img src="../assets/images/header_email.png" alt="" class="header__center_phone-img">
+          info@email.com
         </a>
       </div>
       <div class="header__right flex_1">
@@ -48,8 +52,8 @@ export default {
     opacity: 100%;
 
     &__logo {
-      text-transform: capitalize;
       color: #fff;
+      font-family: Poppins;
 
       &_link {
         display: block;
@@ -57,14 +61,44 @@ export default {
         text-align: center;
       }
       &_top {
+        text-align: start;
+
         font-size: 3.6rem;
+        text-transform: capitalize;
+        letter-spacing: .36rem;
       }
       &_bottom {
-        letter-spacing: 3rem;
+        text-align: end;
+
+        font-size: medium;
+        letter-spacing: 2.95rem;
+        text-transform: uppercase;
       }
     }
     &__center {
-      justify-content: space-around;
+      justify-content: space-between;
+      align-items: center;
+
+      font-size: 1.7rem;
+      &_phone {
+        display: flex;
+        align-items: center;
+
+        font-family: Roboto;
+        text-decoration: none;
+        color: #fff;
+
+        &-img {
+          height: 2.5rem;
+          padding-right: 14px;
+        }
+        &-number {
+          display: flex;
+          & p:not(:last-child) {
+            padding-right: 5px;
+          }
+        }
+      }
     }
     &__right {
       justify-content: flex-end;
