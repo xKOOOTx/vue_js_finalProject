996373bede2b64c3d20621836fd0e55cb65d7803
KOOOT <kooot123@gmail.com>
2020-12-24 03:24:47 +0300

    initial commit

---
+++ game-store/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Header/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: -apple-system,
+  BlinkMacSystemFont,
+  "Segoe UI",
+  Roboto,
+  Oxygen,
+  Ubuntu,
+  Cantarell,
+  "Fira Sans",
+  "Droid Sans",
+  "Helvetica Neue",
+  sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
